9fa4dc4127c83e4ec23d47fcf37a53a11dbab2c5
fh <test>
2019-05-05 22:57:24 +0200

    java infer run works?

--- src/main/java/de/thl/intelijinfer/actions/RunAction.java
+++ src/main/java/de/thl/intelijinfer/actions/RunAction.java
@@ -1,12 +1,20 @@
 package de.thl.intelijinfer.actions;
 
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.PlatformUtils;
+import com.intellij.util.indexing.FileBasedIndex;
 import de.thl.intelijinfer.service.InferRunner;
+
+import java.util.Collection;
 
 public class RunAction extends AnAction {
     public RunAction() {
@@ -15,8 +23,8 @@ public class RunAction extends AnAction {
 
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getProject();
-        InferRunner ir = InferRunner.getInstance(project);
-        ir.run();
+        InferRunner.getInstance(project).run();
+
         //Sdk projectSDK = ProjectRootManager.getInstance(project).getProjectSdk();
         //Messages.showMessageDialog(project, projectSDK.getSdkType().getName(), "Greeting", Messages.getInformationIcon());
     }
--- src/main/java/de/thl/intelijinfer/service/InferRunner.java
+++ src/main/java/de/thl/intelijinfer/service/InferRunner.java
@@ -1,11 +1,18 @@
 package de.thl.intelijinfer.service;
 
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.indexing.FileBasedIndex;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Collection;
 
 public class InferRunner {
     private Project project;
@@ -21,12 +28,30 @@ public class InferRunner {
     public void run() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.directory(new File(project.getBasePath()));
-        processBuilder.command("infer -- javac -d bin " + project.getBasePath() + "/src/*.java");
+        String[] commands = {"/bin/bash", "-c", "infer -- " + createJavacParams()};
+        processBuilder.command(commands);
         try {
             Process p = processBuilder.start();
             p.waitFor();
+            Messages.showMessageDialog(project, "Fertig", "Greeting", Messages.getInformationIcon());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    @NotNull
+    private String createJavacParams() {
+        StringBuilder sb = new StringBuilder("javac -d out/infer ");
+
+        //Get all Class Filepaths
+        Collection<VirtualFile> projectJavaFiles = FileBasedIndex.getInstance().getContainingFiles(
+                FileTypeIndex.NAME, JavaFileType.INSTANCE, GlobalSearchScope.projectScope(project));
+        for (VirtualFile virtualFile : projectJavaFiles) {
+            sb.append(virtualFile.getCanonicalPath() + " ");
+        }
+
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
